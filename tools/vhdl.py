f5b4b93d82be666c90788b5529e37beae44ceec9
Joschka Lingemann <joschka.lingemann@cern.ch>
2014-05-06 15:36:41 +0200

    Added vectors to possible values of vhdl parser.

--- tools/vhdl.py
+++ tools/vhdl.py
@@ -2,6 +2,8 @@
 import re
 from logger import log
 
+
+#constant MU_QUAD_ASSIGNMENT : QuadAssignment_vector(8 downto 0) := (17, 16, 15, 14, 13, 12, 11, 10, 9);
 class VHDLConstantsParser(object):
     """A parser that understands only constant definitions"""
     _log = log.init_logging("vhdl parse")
@@ -29,6 +31,7 @@ class VHDLConstantsParser(object):
         f = open(filename, "r")
         re_vname = re.compile("constant +[A-Za-z_0-9]*")
         re_value = re.compile(":= *[0-9]+")
+        re_type = re.compile(": *[A-Za-z_0-9]*")
         re_vcomb = re.compile(":= *[A-Za-z+*_]+")
         config_dict = {}
         for i, line in enumerate(f):
@@ -43,13 +46,24 @@ class VHDLConstantsParser(object):
                 else:
                     VHDLConstantsParser._log.error("constant in line but no variable name in line {}".format(i+1))
                     continue
+                type_match = re_type.search(line)
                 value_match = re_value.search(line)
                 vcomb_match = re_vcomb.search(line)
-                if value_match:
-                    config_dict[vname] = int(re.split(r":= *", value_match.group(0))[1])
-                elif vcomb_match:
-                    config_dict[vname] = VHDLConstantsParser.get_actual_value(vcomb_match.group(0), config_dict)
-                elif vname:
-                    VHDLConstantsParser._log.error("variable name but value not understandable in line {}".format(i+1))
 
+                if type_match:
+                    t = re.split(r": *", type_match.group(0))[1]
+                    print t
+                    if t in ["integer", "natural"]:                
+                        if value_match:
+                            config_dict[vname] = int(re.split(r":= *", value_match.group(0))[1])
+                        elif vcomb_match:
+                            config_dict[vname] = VHDLConstantsParser.get_actual_value(vcomb_match.group(0), config_dict)
+                        elif vname:
+                            VHDLConstantsParser._log.error("variable name but value not understandable in line {}".format(i+1))
+                    if "vector" in t:
+                        vals = line.split("(")[2].replace(")", "").replace(";", "")
+                        vals = vals.split(",")
+                        vals = [ int(x.strip()) for x in vals]
+                        vals.reverse()
+                        config_dict[vname] = vals 
         return config_dict
