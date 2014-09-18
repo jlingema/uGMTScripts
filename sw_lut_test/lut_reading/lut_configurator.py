9d3719ba2cdc43ab0c6072db5d51ff65ad783adb
Joschka Lingemann <joschka.lingemann@cern.ch>
2014-09-18 16:28:12 +0200

    Changes to the LUT-configurator, using the correct input as LSW/MSW

--- sw_lut_test/lut_reading/lut_configurator.py
+++ sw_lut_test/lut_reading/lut_configurator.py
@@ -1,27 +1,80 @@
-import json
+import json, math, sys
 
 class LUTConfigurator(object):
     """A class that can parse LUT description json files"""
-    def __init__(self, fname):
+    def __init__(self, fname = None):
         super(LUTConfigurator, self).__init__()
         self.fname = fname
         json_file = None
-        try:
-            json_file = open(fname, "r")
-        except:
-            print "could not open json {fname}".format(fname=fname)
-        try:
-            self._json = json.load(json_file)
-        except:
-            print "could not parse json {fname}".format(fname=fname)
+        if fname != None:
+            try:
+                json_file = open(fname, "r")
+            except:
+                print "could not open json {fname}".format(fname=fname)
+            try:
+                self._json = json.load(json_file)
+            except:
+                print "could not parse json {fname}".format(fname=fname)
+        else:
+            self._json = None
+
+        self.phimap = []
+        muon_phi_stepsize = 2*math.pi / pow(2, 10)
+        tower_phi_size = 2*0.087
+        for x in xrange(pow(2, 10)): #phi
+            self.phimap.append(int(x*muon_phi_stepsize / tower_phi_size))
+
+        
+        self.towermap = []
+        self.create_tower_map()
+
+        self.etamap = {}
+        muon_eta_stepsize = 0.011
+        neg_offset = 0b100000000
+        for x in xrange(pow(2, 8)):
+            eta_mu = muon_eta_stepsize*x
+            twr_idx_neg, twr_idx_pos = self.get_tower_index(eta_mu)
+            self.etamap[x] = twr_idx_pos
+            self.etamap[x+neg_offset] = twr_idx_neg
+        
+
+    def create_tower_map(self):
+        if self.towermap == []:
+            self.towermap = []
+            i1 = 0
+            for i in range(10):
+                i1 += 0.087*2
+                self.towermap.append(0.087*2)
+            self.towermap.append(0.19)
+
+            self.towermap.append(0.242)
+
+            self.towermap.append(0.15)
+            self.towermap.append(0.178)
+            self.towermap.append(0.15)
+            self.towermap.append(0.35)
+
+
+    def get_tower_index(self, eta):
+        if eta > 2.4: return 0, 0
+        eta_tot = 0
+        i = 0
+        while (eta_tot < eta):
+            eta_tot += self.towermap[i]
+            i += 1
+        return 14-i, 15+i 
 
     def get_lut(self, lut_path):
-        path_parts = lut_path.split(".")[1:]
+        if self._json:
+            path_parts = lut_path.split(".")[1:]
 
-        curr = self._json[path_parts[0]]
-        for path_part in path_parts[1:]:
-            curr = curr[path_part]
-        return curr
+            curr = self._json[path_parts[0]]
+            for path_part in path_parts[1:]:
+                curr = curr[path_part]
+            return curr
+        else:
+            print "[{name}] ERROR : trying to access json configuration not specified during construction".format(name=self.__class__)
+            sys.exit(0)
 
     def get_lut_data(self, lut_path):
         lut = self.get_lut(lut_path)
@@ -58,6 +111,11 @@ class LUTConfigurator(object):
         from math import sin, pi
         data = [ int((1+sin(x/float(max_addr)*2*pi))*max_data_val) for x in range(max_addr) ]
         return data
+    @staticmethod
+    def generate_mask(xlow, xup):
+        ones = (1<<(xup-xlow+1)) - 1
+        mask = ones<<xlow
+        return mask
 
     @staticmethod
     def get_2param_function_lut(function_string, addr_width1, addr_width2, data_width):
@@ -73,7 +131,7 @@ class LUTConfigurator(object):
         for x in xrange(max_addr):
             # unhash "input"
             a = x & mask1
-            b = (x & mask2) >> addr_width1
+            b = (x & mask2) << addr_width1
             # take maximum data if overflow
             res = min(eval(function_string), data_max)
             data.append(res)
@@ -81,30 +139,58 @@ class LUTConfigurator(object):
 
     @staticmethod
     def get_function_lut(function_string, input_names, addr_widths, data_width):
-        max_addr = pow(2, sum(addr_widths))
+        max_addr = 1 << (sum(addr_widths))
+
+        #the convention is that the first input is the MSW, therefore for simplicity, we just reverse the lists here:
+        rev_input_names = [ x for x in input_names ]
+        rev_input_names.reverse()
+        rev_addr_widths = [ x for x in addr_widths ]
+        rev_addr_widths.reverse()
+        
         # prepare the function string to use the local list inputs (see below)
-        for i, input_name in enumerate(input_names):
-            function_string = function_string.replace(input_name, "inputs[{idx}]".format(idx=i))
+        my_function_string = function_string
+        for i, input_name in enumerate(rev_input_names):
+            my_function_string = my_function_string.replace(input_name, "inputs[{idx}]".format(idx=i))
+
+        # making my life easier by calculating the bit boundaries from the widths:
+        bit_boundaries = []
+        bitsum = 0
+        for addw in rev_addr_widths:
+            bit_boundaries.append([bitsum, bitsum+addw-1])
+            bitsum += addw
 
         # prepare the masks according to the address widths:
         masks = []
-        for i, addr_width in enumerate(addr_widths):
-            mask = (1 << addr_width)
-            if i > 0:
-                mask = mask << addr_widths[i-1]
-            mask -= 1
-            masks.append(mask)
+        for i, boundary in enumerate(bit_boundaries):
+            msk = LUTConfigurator.generate_mask(boundary[0], boundary[1])
+            print rev_input_names[i], "mask:", bin(msk)
+            masks.append(msk)
 
-        data_max = pow(2, data_width) - 1
+        data_max = (1 << data_width) - 1 
         data = []
         for x in xrange(max_addr):
             # unhash "input"
             inputs = []
+
             for i, mask in enumerate(masks):
                 inputs.append(x & mask)
-                if i > 0:
-                    inputs[i] = inputs[i] >> addr_widths[i-1]
+                inputs[i] = inputs[i] >> bit_boundaries[i][0] # shift by lower bound
             # take maximum data if overflow
-            res = min(eval(function_string), data_max)
+            res = min(eval(my_function_string), data_max)
+
+            data.append(res)
+        return data
+
+    def get_map_lut(self, mapname, input_name, addr_width, data_width):
+        max_addr = pow(2, addr_width)
+        # prepare the function string to use the local list inputs (see below)
+        lookupstring = "self."+mapname+"[input_]"
+
+
+        data_max = pow(2, data_width) - 1
+        data = []
+        for input_ in xrange(max_addr):
+            # take maximum data if overflow
+            res = min(eval(lookupstring), data_max)
             data.append(res)
         return data
--- sw_lut_test/lut_reading/lut_file_generator.py
+++ sw_lut_test/lut_reading/lut_file_generator.py
@@ -3,13 +3,21 @@ import json
 from lut_configurator import LUTConfigurator
 from datetime import date
 
-coe_header = """; Auto-generated Memory-Initialization .COE file for uGMT bench-tests
+coe_header = """; Auto-generated Memory-Initialization .COE file for Core Generator, used in uGMT bench-tests
 ; LUT:        {lutname}
 ; Function:   {function}
 ; Version:    {version}
 ; Generated:  {date}
 memory_initialization_radix=16;
 memory_initialization_vector="""
+
+emu_header = """# Auto-generated Memory-Initialization .lut file for uGMT emulator-tests
+# LUT:        {lutname}
+# Function:   {function}
+# Version:    {version}
+# Generated:  {date}
+CONTENT_VECTOR= """
+
 
 def write_coe_file(name, dirname, function, data):
     with open("{dirname}/{fname}.coe".format(dirname=dirname, fname=name), "w") as coe_file:
@@ -21,6 +29,12 @@ def write_coe_file(name, dirname, function, data):
             else:
                 coe_file.write(";")
 
+def write_emu_lut_file(name, dirname, function, data):
+    with open("{dirname}/{fname}.lut".format(dirname=dirname, fname=name), "w") as lut_file:
+        lut_file.write(emu_header.format(lutname=name, function=function, version=1, date=date.today()))
+        for i, d in enumerate(data):
+            lut_file.write("{datum} ".format(datum=int(d)))
+
 def parse_options():
     '''
     Parse the command line options
@@ -31,10 +45,12 @@ def parse_options():
 
 '''
     defaults = {
-        'outdir'       : 'data/coe/',
+        'coe_out'       : 'data/coe/',
+        'emu_out'       : 'data/emu/',
     }
     parser = optparse.OptionParser( usage )
-    parser.add_option('-o', '--outdir'   , dest='outdir'   , help='location for output files (%default)', default=defaults['outdir']   , type='string' )
+    parser.add_option('--coe'   , dest='coe_out'   , help='location for output files (%default)', default=defaults['coe_out']   , type='string' )
+    parser.add_option('--emu'   , dest='emu_out'   , help='location for output files (%default)', default=defaults['emu_out']   , type='string' )
 
     opts, args = parser.parse_args()
 
@@ -45,17 +61,23 @@ def parse_options():
 
     return opts,args
 
+ETAMAP = {}
+PHIMAP = []
+
 if __name__ == "__main__":
     opts, args = parse_options()
-   
+    lut_cfger = LUTConfigurator()
     with open(opts.json_name, 'r') as json_file:
         lut_config = json.load(json_file)
-
         for name, config in lut_config.iteritems():
             if config["function"] != "":
                 lut_data = LUTConfigurator.get_function_lut(config["function"], config["inputs"], config["input_widths"], config["data_out_width"])
-                write_coe_file(name, opts.outdir, config["function"], lut_data)
-
-
+                write_coe_file(name, opts.coe_out, config["function"], lut_data)
+                write_emu_lut_file(name, opts.emu_out, config["function"], lut_data)
+            elif config["map"]:
+                if len(config["inputs"]) > 1: print "ERROR : Map only works with one input, currently"
+                lut_data = lut_cfger.get_map_lut(config["map"], config["inputs"][0], config["input_widths"][0], config["data_out_width"])
+                write_coe_file(name, opts.coe_out, config["function"], lut_data)
+                write_emu_lut_file(name, opts.emu_out, config["function"], lut_data)
     
 
