094b522dc5bd2334c0be8017fb56336cb460c442
Joschka Lingemann <joschka.lingemann@cern.ch>
2014-11-11 10:50:11 +0100

    Added a first version of test patterns for the serializer test-bench, as discussed in #13.

--- ugmt_patterns/dat_to_testbench.py
+++ ugmt_patterns/dat_to_testbench.py
@@ -25,10 +25,10 @@ from helpers.options import parse_options, discover_files
 
 
 
-def get_header():
+def get_header_algo():
     string = "#"*80
     string += """
-# Testbench file for the uGMT
+# Pattern for testbench of the uGMT algo block
 # Data format of muons:
 # ID N PT PHI ETA CHARGE CHARGE_VALID QUALITY SORT EMPTY (ISO)
 # where ID = {FWD+/-, OVL+/-, BAR, OUT, FIMD, BIMD, OIMD}
@@ -41,6 +41,26 @@ def get_header():
 # Tracks are given always for 3 consecutive muons
 """
     return string
+
+def get_header_serializer():
+    string = "#"*80
+    string += """
+# Pattern for testbench of the uGMT serializer
+# Data format of muons:
+# ID N PT PHI ETA CHARGE CHARGE_VALID QUALITY SORT EMPTY (ISO)
+# where ID = {FWD+/-, OVL+/-, BAR, OUT, FIMD, BIMD, OIMD}
+# N is the rank for IMD / OUT and the link for inputs.
+# ISO is optional and only present for OUT
+# 
+# Data format for output of serializer:
+# ID VALID0 WORD0 VALID1 WORD1 ... VALIDN WORDN
+# where ID = FRMx, x being the current 240 MHz cycle
+# In WordX the X represents the link number, meaning that muon0 is x(FRM0, WORD0) + x(FRM1, WORD0) << 32.
+"""
+    return string
+
+
+
 
 def link_from_phi(phi, mutype):
     """
@@ -105,6 +125,46 @@ def get_muon_lines(mu_list, mu_type, addIso):
             else: themuid = "BIMD"
         string += get_muon_line(mu, themuid, i, addIso)
     
+    return string
+
+def get_muon_words(imd_mu_list, out_mu_list):
+    string = ""
+    n_words_per_link = 6 # 6 240 MHz cycles in 40 MHz clock
+    #msw_mask = 0xffffffff00000000
+    lsw_mask = 0xffffffff
+
+    out_words = [ [] for x in range(12)]# imd = 8; out = 4
+    for i, mu in enumerate(out_mu_list):
+        link = i / 2
+        if i%2 == 0: # fill padding words
+            out_words[link].append(0)
+            out_words[link].append(0)
+        bitword = mu.bitword
+        msw = bitword >> 32
+        lsw = bitword & lsw_mask
+        out_words[link].append(lsw)
+        out_words[link].append(msw)
+
+    for i, mu in enumerate(imd_mu_list):
+        link = i / 3+4
+        bitword = mu.bitword
+        msw = bitword >> 32
+        lsw = bitword & lsw_mask
+        out_words[link].append(lsw)
+        out_words[link].append(msw)
+
+    string += "{id:<7} ".format(id="ID")
+    for link in range(12): string += "{valid:>2} {word:>11} ".format(valid="V"+str(link), word="LINK"+str(link))
+    string += "\n"
+    for frm in range(n_words_per_link):
+        string += "{n:<7} ".format(n="FRM"+str(frm))
+        for link in range(12):
+            valid = 1
+            if frm < 2 and link < 4: valid = 0
+            string += "{valid:>2} {word:>11} ".format(valid=valid, word=out_words[link][frm])
+
+        string += "\n"
+
     return string
 
 def convert_input_vector_to_strings(vec, vhdl_dict, mu_type, size, rankLUT):
@@ -184,88 +244,110 @@ def main() :
         fwd_handle = Handle('std::vector<l1t::L1TRegionalMuonCandidate>')
         ovl_handle = Handle('std::vector<l1t::L1TRegionalMuonCandidate>')
 
-        out_fname = "data/patterns/testbench/{pname}.txt".format(pname = pattern)
-        with open(out_fname, 'w') as out_file:
-            out_file.write(get_header())
-            for i, event in enumerate(events):
-                out_file.write("#"*80+"\n")
-                out_file.write("# Event: {ievent}\n".format(ievent=i))
-                out_file.write("#"*80+"\n")
+        algo_fname = "data/patterns/testbench/{pname}.txt".format(pname = pattern)
+        ser_fname = "data/patterns/testbench/serializer_{pname}.txt".format(pname = pattern)
 
-                event.getByLabel("microGMTEmulator", out_handle)
-                event.getByLabel("microGMTEmulator", "intermediateMuons", imd_handle)
-                event.getByLabel("uGMTInputProducer", "BarrelTFMuons", bar_handle)
-                event.getByLabel("uGMTInputProducer", "ForwardTFMuons", fwd_handle)
-                event.getByLabel("uGMTInputProducer", "OverlapTFMuons", ovl_handle)
+        algo_string = get_header_algo()
+        ser_string = get_header_serializer()
 
-                emu_out_muons = out_handle.product()
-                imd_prod = imd_handle.product()
-                emu_bar_muons = bar_handle.product()
-                emu_ovl_muons = ovl_handle.product()
-                emu_fwd_muons = fwd_handle.product()
+        for i, event in enumerate(events):
+            event_head = "#"*80+"\n"
+            event_head += "# Event: {ievent}\n".format(ievent=i)
+            event_head += "#"*80+"\n"
 
-                table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5}\n".format(
-                    id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT")
-                out_file.write(table_head)
+            algo_string += event_head
+            ser_string += event_head
 
-                # Get all input muon / track strins and write the muons to file
-                muon_string_fwdp, track_string_fwdp = convert_input_vector_to_strings(emu_fwd_muons, vhdl_dict, "FWD+", 18, rankLUT)
-                out_file.write(muon_string_fwdp)
-                muon_string_ovlp, track_string_ovlp = convert_input_vector_to_strings(emu_ovl_muons, vhdl_dict, "OVL+", 18, rankLUT)
-                out_file.write(muon_string_ovlp)
-                muon_string_bar, track_string_bar = convert_input_vector_to_strings(emu_bar_muons, vhdl_dict, "BAR", 36, rankLUT)
-                out_file.write(muon_string_bar)
-                muon_string_ovln, track_string_ovln = convert_input_vector_to_strings(emu_ovl_muons, vhdl_dict, "OVL-", 18, rankLUT)
-                out_file.write(muon_string_ovln)
-                muon_string_fwdn, track_string_fwdn = convert_input_vector_to_strings(emu_fwd_muons, vhdl_dict, "FWD-", 18, rankLUT)
-                out_file.write(muon_string_fwdn)
+            event.getByLabel("microGMTEmulator", out_handle)
+            event.getByLabel("microGMTEmulator", "intermediateMuons", imd_handle)
+            event.getByLabel("uGMTInputProducer", "BarrelTFMuons", bar_handle)
+            event.getByLabel("uGMTInputProducer", "ForwardTFMuons", fwd_handle)
+            event.getByLabel("uGMTInputProducer", "OverlapTFMuons", ovl_handle)
 
-                table_head = "#{id:<5}".format(id="TYPE")
-                for i in range(3):
-                    table_head += " {eta:>5} {phi:>5} {quality:>5}".format(eta="ETA"+str(i),
-                                                                           phi="PHI"+str(i),
-                                                                           quality="QUAL"+str(i) )
-                out_file.write(table_head+"\n")
-                # Now write the tracks 
-                out_file.write(track_string_fwdp)
-                out_file.write(track_string_ovlp)
-                out_file.write(track_string_bar)
-                out_file.write(track_string_ovln)
-                out_file.write(track_string_fwdn)
+            emu_out_muons = out_handle.product()
+            imd_prod = imd_handle.product()
+            emu_bar_muons = bar_handle.product()
+            emu_ovl_muons = ovl_handle.product()
+            emu_fwd_muons = fwd_handle.product()
 
-                out_file.write("#"*80+"\n")
-                out_file.write("# Emulator information for event: {ievent}\n".format(ievent=i))
-                out_file.write("#"*80+"\n")
+            table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5}\n".format(
+                id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT")
+            algo_string += table_head
 
-                # first convert the output / intermediate muons into simple py-litst
-                out_list = []
-                for j in xrange(emu_out_muons.size(0)):
-                    mu = emu_out_muons.at(0, j)
-                    mu_tmp = Muon(vhdl_dict, mu_type="OUT", obj=mu)
-                    out_list.append(mu_tmp)
-                add_muons = 8-emu_out_muons.size(0)
-                while add_muons != 0:
-                    mu_tmp = Muon(vhdl_dict, mu_type="OUT", bitword=0)    
-                    out_list.append(mu_tmp)
-                    add_muons -= 1
+            # Get all input muon / track strins and write the muons to file
+            muon_string_fwdp, track_string_fwdp = convert_input_vector_to_strings(emu_fwd_muons, vhdl_dict, "FWD+", 18, rankLUT)
+            algo_string += muon_string_fwdp
+            muon_string_ovlp, track_string_ovlp = convert_input_vector_to_strings(emu_ovl_muons, vhdl_dict, "OVL+", 18, rankLUT)
+            algo_string += muon_string_ovlp
+            muon_string_bar, track_string_bar = convert_input_vector_to_strings(emu_bar_muons, vhdl_dict, "BAR", 36, rankLUT)
+            algo_string += muon_string_bar
+            muon_string_ovln, track_string_ovln = convert_input_vector_to_strings(emu_ovl_muons, vhdl_dict, "OVL-", 18, rankLUT)
+            algo_string += muon_string_ovln
+            muon_string_fwdn, track_string_fwdn = convert_input_vector_to_strings(emu_fwd_muons, vhdl_dict, "FWD-", 18, rankLUT)
+            algo_string += muon_string_fwdn
 
-                imd_list = []
-                for j in xrange(imd_prod.size(0)):
-                    mu = imd_prod.at(0, j)
-                    mu_tmp = Muon(vhdl_dict, mu_type="OUT", obj=mu)
-                    imd_list.append(mu_tmp)
+            table_head = "#{id:<5}".format(id="TYPE")
+            for i in range(3):
+                table_head += " {eta:>5} {phi:>5} {quality:>5}".format(eta="ETA"+str(i),
+                                                                       phi="PHI"+str(i),
+                                                                       quality="QUAL"+str(i) )
+            algo_string += table_head+"\n"
+            # Now write the tracks 
+            algo_string += track_string_fwdp
+            algo_string += track_string_ovlp
+            algo_string += track_string_bar
+            algo_string += track_string_ovln
+            algo_string += track_string_fwdn
 
-                # convert them into strings and write to file
-                out_muons_str = get_muon_lines(out_list, mu_type="OUT", addIso=True)
-                imd_muons_str = get_muon_lines(imd_list, mu_type="IMD", addIso=False)
-                table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5}\n".format(
-                    id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT")
-                out_file.write(table_head)
-                out_file.write(imd_muons_str)
-                table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5} {iso:>5}\n".format(
-                    id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT", iso="ISO")
-                out_file.write(table_head)
-                out_file.write(out_muons_str)
+            algo_string += "#"*80+"\n"
+            algo_string += "# Emulator information for event: {ievent}\n".format(ievent=i)
+            algo_string += "#"*80+"\n"
+
+            # first convert the output / intermediate muons into simple py-litst
+            out_list = []
+            for j in xrange(emu_out_muons.size(0)):
+                mu = emu_out_muons.at(0, j)
+                mu_tmp = Muon(vhdl_dict, mu_type="OUT", obj=mu)
+                out_list.append(mu_tmp)
+            add_muons = 8-emu_out_muons.size(0)
+            while add_muons != 0:
+                mu_tmp = Muon(vhdl_dict, mu_type="OUT", bitword=0)    
+                out_list.append(mu_tmp)
+                add_muons -= 1
+
+            imd_list = []
+            for j in xrange(imd_prod.size(0)):
+                mu = imd_prod.at(0, j)
+                mu_tmp = Muon(vhdl_dict, mu_type="OUT", obj=mu)
+                imd_list.append(mu_tmp)
+
+            # convert them into strings and write to file
+            out_muons_str = get_muon_lines(out_list, mu_type="OUT", addIso=True)
+            imd_muons_str = get_muon_lines(imd_list, mu_type="IMD", addIso=False)
+            table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5}\n".format(
+                id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT")
+            algo_string += table_head
+            algo_string += imd_muons_str
+            ser_string += table_head
+            ser_string += imd_muons_str
+            table_head = "#{id:<5} {rank:>5} {pt:>5} {phi:>5} {eta:>5} {charge:>5} {charge_valid:>5} {quality:>5} {sort:>5} {empty:>5} {iso:>5}\n".format(
+                id="TYPE", rank="POS", pt="PT", phi="PHI", eta="ETA", charge = "SIGN", charge_valid="VSIG", quality="QUAL", sort="RANK", empty="EMPT", iso="ISO")
+            algo_string += table_head
+            algo_string += out_muons_str
+            ser_string += table_head
+            ser_string += out_muons_str
+
+            ser_string += "#"*80+"\n"
+            ser_string += "# Expected serializer output for event: {ievent}\n".format(ievent=i)
+            ser_string += "#"*80+"\n"
+
+            ser_string += get_muon_words(imd_list, out_list)
+
+        with open(algo_fname, 'w') as algo_file:
+            algo_file.write(algo_string)
+
+        with open(ser_fname, 'w') as ser_file:
+            ser_file.write(ser_string)
 
 if __name__ == "__main__":
     main()
