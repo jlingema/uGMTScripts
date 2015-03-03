a28c489efd97128ad0be6060389eecb8375883e1
Joschka Lingemann <joschka.lingemann@cern.ch>
2015-03-03 13:00:21 +0100

    Bug-fix: default was still as per optparse.

--- ugmt_patterns/helpers/options.py
+++ ugmt_patterns/helpers/options.py
@@ -12,16 +12,16 @@ def parse_options(description = ""):
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("--directory", dest="directory", type=str, help="Directory containing buffer dumps.")
     parser.add_argument("--emudirectory", dest="emudirectory", type=str, default="data/patterns/mp7", help="Directory containing emulator output root-files.")
-    parser.add_argument("--nodebug", dest="nodebug", help='Whether debug output is in the file (intermediate muons, ranks and calo energies) (%default)', default=False, action='store_true')
-    parser.add_argument('--verbose', dest='verbose', help='Additional output about muons per event (%default)', default=False, action='store_true')
-    parser.add_argument('--veryverbose', dest="detaildump", help="Even more output (%defahult)", default=False, action="store_true")
+    parser.add_argument("--nodebug", dest="nodebug", help='Whether debug output is in the file (intermediate muons, ranks and calo energies)', default=False, action='store_true')
+    parser.add_argument('--verbose', dest='verbose', help='Additional output about muons per event', default=False, action='store_true')
+    parser.add_argument('--veryverbose', dest="detaildump", help="Even more output", default=False, action="store_true")
     parser.add_argument("--delay", dest="delay", help="Specify how many empty frames should be written to mp7-pattern", default=4, type=int)
     parser.add_argument("--gtdumps", dest="gtdumps", help="Specify a folder containing files that represent the GT-spy buffer dumps", default="", type=str)
     parser.add_argument("--gtoffset", dest="gtoffset", help="Specify the offset for the GT-spy buffer (leading comma)", default=31, type=int)
     
     opts = parser.parse_args()
     
-    if opts.detaildump: opts.verbose = True
+    # if opts.detaildump: opts.verbose = True
     return opts
 
 def discover_files(opts):
