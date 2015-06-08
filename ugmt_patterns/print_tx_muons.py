689a9b7ca5ba05aeace29d1381c0d0771ec2d284
Joschka Lingemann <joschka.lingemann@cern.ch>
2015-06-08 20:44:34 +0200

    Added comparison functionality.

--- ugmt_patterns/print_tx_muons.py
+++ ugmt_patterns/print_tx_muons.py
@@ -16,17 +16,21 @@ if __name__ == "__main__":
         # Reading and processing the hardware data
         output_parser = OutputBufferParser(fnames["tx"], vhdl_dict, version)
         out_muons = output_parser.get_output_muons()
-        non_zero = [ mu for mu in out_muons if mu.bitword != 0 ]
+        imd_muons = output_parser.get_intermediate_muons()
+        non_zero = [mu for mu in out_muons if mu.bitword != 0]
 
         out_muons_emu = None
         if "emu_tx" in fnames.keys():
             output_parser_emu = OutputBufferParser(fnames["emu_tx"], vhdl_dict, version)
-            out_muons_emu = output_parser_emu.get_output_muons()            
-            non_zero_emu = [ mu for mu in out_muons_emu if mu.bitword != 0 ]
+            imd_muons_emu = output_parser_emu.get_intermediate_muons()
+            out_muons_emu = output_parser_emu.get_output_muons()
+            non_zero_emu = [mu for mu in out_muons_emu if mu.bitword != 0]
 
 
         if out_muons_emu != None:
             mu_to_print = {}
+            imd_to_print = zip(imd_muons, imd_muons_emu)
+            print imd_to_print
             if options.verbose:
                 mu_to_print = zip(out_muons, out_muons_emu)
             else:
@@ -35,6 +39,9 @@ if __name__ == "__main__":
             last_bx = 0
             for mu, mu_emu in mu_to_print:
                 if mu.bx != last_bx:
+                    print "*"*50, "intermediates", "*"*50
+                    for i in range(last_bx*24, last_bx*24 + 24):
+                        print "hw", print_out_word(imd_to_print[i][0].bitword), "emu", print_out_word(imd_to_print[i][1].bitword)
                     print "*"*50, "current bx:", mu.bx, "*"*50
                     last_bx = mu.bx
                 if options.verbose or (mu.bitword != mu_emu.bitword):
@@ -46,7 +53,7 @@ if __name__ == "__main__":
             else:
                 mu_to_print = non_zero
 
-            last_bx = 0            
+            last_bx = 0
             for mu in non_zero:
                 if mu.bx != last_bx:
                     print "*"*50, "current bx:", mu.bx, "*"*50
